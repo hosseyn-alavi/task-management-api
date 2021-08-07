b44237d66b5b0736915292c14eb6c5543a1ed7db
Hosseyn <s.m.hosseyn.alavi@gmail.com>
2021-08-07 09:07:50 +0200

    handle duplicate user signup

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -11,7 +15,15 @@ export class UserRepository extends Repository<User> {
       username,
       password,
     });
-
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        //duplicate error in postgres
+        throw new ConflictException('Username already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
